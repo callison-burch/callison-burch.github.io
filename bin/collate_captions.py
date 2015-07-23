8a21b8f2fe14459a7ea7bc2341ca3376800a02fe
Chris Callison-Burch <ccb@stanfordalumni.org>
2015-07-22 23:06:53 -0400

    Update the YAML file inline

--- bin/collate_captions.py
+++ bin/collate_captions.py
@@ -24,24 +24,32 @@ with open('captions.csv', 'rb') as csvfile:
 				doc_info[id][type + " " + num] = caption
 
 
-path_to_publications = "/Users/ccb/Sites/callison-burch.github.io/publications/"
-for id in doc_info:
-	print id
-	full_text = " ".join(list(open(path_to_publications + id + ".txt")))
+publications_data = list(open("/Users/ccb/Sites/callison-burch.github.io/_data/publications.yaml"))
 
-	uniq_items = set()
-	ordered_items = []
-	results = re.findall("Table \d+|Figure \d+", full_text)
-	for item in results:
-		if item not in uniq_items:
-			uniq_items.add(item)
-			ordered_items.append(item)
-	for item in ordered_items:
-		if item in doc_info[id]:
-			caption = doc_info[id][item]
-			print "         img: figures/" + id + "/" + id + "-" + item.lower().replace(" ", "-") + ".jpg"
-			print "         label: " + item
-			print "         caption: " + caption.replace(":", "&colon;")
+path_to_publications = "/Users/ccb/Sites/callison-burch.github.io/publications/"
+
+for line in publications_data:
+	print line,
+	if line.startswith("   id: "):
+		id = line[7:len(line)-1]
+		if id in doc_info:
+			print "   figures:"
+			print "      -"
+			full_text = " ".join(list(open(path_to_publications + id + ".txt")))
+
+			uniq_items = set()
+			ordered_items = []
+			results = re.findall("Table \d+|Figure \d+", full_text)
+			for item in results:
+				if item not in uniq_items:
+					uniq_items.add(item)
+					ordered_items.append(item)
+			for item in ordered_items:
+		       		if item in doc_info[id]:
+		       			caption = doc_info[id][item]
+		       			print "         img: figures/" + id + "/" + id + "-" + item.lower().replace(" ", "-") + ".jpg"
+		       			print "         label: " + item
+		       			print "         caption: " + caption.replace(":", "&colon;")
 
 
 
